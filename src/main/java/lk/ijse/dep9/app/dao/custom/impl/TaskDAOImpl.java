c8a6f0764b833b9a2e4131a94b2e98352656e068
Pubudu Janith <pubudujanith123@gmail.com>
2022-12-20 17:13:32 +0530

    Finalize the ProjectDAOImpl

--- src/main/java/lk/ijse/dep9/app/dao/custom/impl/TaskDAOImpl.java
+++ src/main/java/lk/ijse/dep9/app/dao/custom/impl/TaskDAOImpl.java
@@ -4,6 +4,8 @@ import lk.ijse.dep9.app.dao.custom.TaskDAO;
 import lk.ijse.dep9.app.entity.Task;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,7 +19,18 @@ public class TaskDAOImpl implements TaskDAO {
 
     @Override
     public Task save(Task task) {
-        return null;
+        try {
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO Task (id, content, status, project_id) VALUES (?,?,?,?)");
+            stm.setInt(1, task.getId());
+            stm.setString(2,task.getContent());
+//            stm.set(3,task.getStatus());
+            stm.setInt(3,task.getProjectId());
+            stm.executeUpdate();
+            return task;
+        }
+        catch (SQLException e) {
+            throw new RuntimeException();
+        }
     }
 
     @Override
